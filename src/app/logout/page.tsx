fca462c279ef6793a59c8142e056fd92fc3ae4b0
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2025-08-23 22:45:37 +0300

    dynamic

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,6 +1,8 @@
-// app/logout/page.tsx
-import { redirect } from "next/navigation";
+// app/(auth)/logout/page.tsx
+export const dynamic = "force-dynamic";
+
 import { signOut } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function LogoutPage() {
   await signOut({ redirect: false });
