e0b72f8bfa6999230959b6c61cdd05dd6cfcb693
hamza <hamza.mohamed3737@gmail.com>
2025-09-20 20:28:50 +0300

    test

--- src/lib/apis/course.api.ts
+++ src/lib/apis/course.api.ts
@@ -1,7 +1,7 @@
 "use server";
 
-import { redirect } from "next/navigation";
 import { getAuthToken } from "../utils/auth-token";
+import { redirect } from "next/navigation";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -15,16 +15,13 @@ export async function GetCourse({
   const response = await fetch(`${API_URL}api/v1/get-course/${course_id}`, {
     method: "GET",
     headers: {
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
-      type: "web",
-      Accept: "application/json",
     },
   });
 
   const payload = await response.json();
-  if (payload?.message === "Unauthenticated.") {
-    redirect("/logout");
-  }
+
   if (!payload || !payload.data) {
     throw new Error("فشل في جلب البيانات");
   }
@@ -81,36 +78,23 @@ export async function GetUnit({
   unit_id: string;
 }): Promise<APIResponse<CourseDetails>> {
   const token = await getAuthToken();
+  console.log("token", token);
   const response = await fetch(`${API_URL}api/v1/get-unit/${unit_id}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
-      type: "web",
-      Accept: "application/json",
     },
   });
 
   const payload = await response.json();
 
-  if (payload?.message === "Unauthenticated.") {
-    redirect("/logout");
-  }
-  if (!response.ok) {
-    throw new Error(
-      payload?.message || payload?.error || "فشل في جلب البيانات"
-    );
-  }
-
   if (!payload || !payload.data) {
-    throw new Error(
-      payload?.message || payload?.error || "فشل في جلب البيانات"
-    );
+    throw new Error("فشل في جلب البيانات");
   }
 
   return payload;
 }
-
 export async function GetLessons({
   course_id,
 }: {
@@ -139,7 +123,6 @@ export async function GetLesson({
   lesson_id: string;
 }): Promise<APIResponse<LessonDetails>> {
   const token = await getAuthToken();
-
   const response = await fetch(`${API_URL}api/v1/lesson/${lesson_id}`, {
     method: "GET",
     headers: {
@@ -149,15 +132,9 @@ export async function GetLesson({
   });
 
   const payload = await response.json();
-
-  if (!response.ok) {
-    throw new Error(payload?.message || "حدث خطأ أثناء جلب الدرس");
+  if (payload?.message === "Unauthenticated.") {
+    redirect("/logout");
   }
-
-  if (payload?.error) {
-    throw payload.error;
-  }
-
   return payload;
 }
 
